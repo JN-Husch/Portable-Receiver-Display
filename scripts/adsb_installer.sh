4d91138b2fb2a3107a8f6dd623c2fe8db8389155
JN-Husch <mail@jn-husch.com>
2024-01-19 00:31:58 +0100

    Added gpsd installation to auto install script

--- scripts/adsb_installer.sh
+++ scripts/adsb_installer.sh
@@ -8,11 +8,34 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Install required Packages
-sudo apt install python3-pip
+echo -e "\033[92m"
+echo "Installing Python3 Pip"
+echo -e "\033[37m"
+sudo apt install python3-pip -y
+echo -e "\033[92m"
+echo "Installing GPSD Client (for the GPS Module)"
+echo -e "\033[37m"
+sudo apt-get install gpsd gpsd-clients -y
+echo -e "\033[92m"
+echo "Installing Pillow"
+echo -e "\033[37m"
 python3 -m pip install --upgrade Pillow --break-system-packages
+echo -e "\033[92m"
+echo "Installing Schedule"
+echo -e "\033[37m"
 pip install schedule --break-system-packages
+echo -e "\033[92m"
+echo "Installing Python Dateutil"
+echo -e "\033[37m"
 pip install python-dateutil --break-system-packages
+echo -e "\033[92m"
+echo "Installing gps3"
+echo -e "\033[37m"
 sudo -H pip3 install gps3 --break-system-packages
+
+echo -e "\033[92m"
+echo "Setting up Serial and SPI Ports"
+echo -e "\033[37m"
 
 # Set up Serial and SPI Communication
 sudo raspi-config nonint do_serial_hw 1 
@@ -20,15 +43,27 @@ sudo raspi-config nonint do_serial_cons 1
 sudo raspi-config nonint do_serial 2
 sudo raspi-config nonint do_spi 0
 
+echo -e "\033[92m"
+echo "Stoping existing PRDS-ADSB service"
+echo -e "\033[37m"
+
 # Remove existing PRDS-ADSB Installation:
 sudo systemctl stop PRDS-ADSB.service
 sudo systemctl disable PRDS-ADSB.service
+
+echo -e "\033[92m"
+echo "Creating PRDS/ADSB directory"
+echo -e "\033[37m"
 
 cd /etc/systemd/system
 sudo rm -r PRDS-ADSB.service
 
 cd /home/pi
 sudo rm -r PRDS
+
+echo -e "\033[92m"
+echo "Getting the PRD Software from Github"
+echo -e "\033[37m"
 
 # Get new PRDS from Github:
 cd /home/pi
@@ -38,8 +73,17 @@ git sparse-checkout set ADSB
 git checkout
 cd ADSB
 
+echo -e "\033[92m"
+echo "Setting up the PRDS-ADSB.service"
+echo -e "\033[37m"
+
 # Set up PRDS-ADSB for automatic start on Pi boot
 sudo cp /home/pi/PRDS/ADSB/PRDS-ADSB.service /etc/systemd/system
 sudo systemctl enable PRDS-ADSB.service
 sudo systemctl start PRDS-ADSB.service
 
+echo -e "\033[92m"
+echo "------------------------------------------------"
+echo "Installation complete!"
+echo -e "\033[37m"
+echo "Please re-boot the Pi by running "sudo reboot"
